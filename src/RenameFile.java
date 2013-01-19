e387df6a0fa818fa877dbb346740a2224a4f3e12
git2000 <lepete2000@yahoo.com>
2013-01-18 23:59:38 -0500

    commit

--- src/RenameFile.java
+++ src/RenameFile.java
@@ -8,44 +8,50 @@ public class RenameFile {
 		// TODO Auto-generated method stub
 		String filename="";
 		File file = null;
+		SimpleDateFormat sdf = null;
 		try {
+			
+		  if (true) {
+				throw new Exception("ERROR");
+		  }
 		  filename = args[0];
  		  file = new File(filename);
 		  if (!file.exists()) {
 			throw new InvalidFileException("Invalid File");
 		  }
+          sdf = new SimpleDateFormat("yyyyMMdd-HHmmss");
+  		  String[] tmp = filename.split("\\.");
+  		  String extension = tmp[tmp.length-1];
+  		
+  		  File file2;
+  	      int idx=1;
+
+	      String newName = sdf.format(file.lastModified()) + "-"+idx+"." + extension;
+	      file2 = new File(newName);
+	      while ((file2.exists()) && (idx < 10)) {
+	    	idx++;
+	    	newName = sdf.format(file.lastModified()) + "-"+idx+"." + extension;
+		    file2 = new File(newName);
+	      }
+	    
+	      boolean success=false;
+	      if(!file2.exists()) {
+  	        success = file.renameTo(file2);
+	      }
+	      if (success) {
+			  System.out.println("Renaming success - " + filename + " to " + newName);
+	      } else {
+			  System.out.println("Renaming error");
+	      }
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Need argument of a valid filename.");
 		} catch (InvalidFileException e) {
 			System.out.println("File ["+filename+"] doesn't exist.");
-			
+		} catch (Exception e ) {
+			System.out.println(e.getMessage());
 		}
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd-HHmmss");
-//		System.out.println("After Format : " + sdf.format(file.lastModified()));
-
-		String[] tmp = filename.split("\\.");
-		String extension = tmp[tmp.length-1];
 		
-		File file2;
-	    int idx=1;
 
-	    String newName = sdf.format(file.lastModified()) + "-"+idx+"." + extension;
-	    file2 = new File(newName);
-	    while ((file2.exists()) && (idx < 10)) {
-	    	idx++;
-	    	newName = sdf.format(file.lastModified()) + "-"+idx+"." + extension;
-		    file2 = new File(newName);
-	    }
-	    
-	    boolean success=false;
-	    if(!file2.exists()) {
-  	      success = file.renameTo(file2);
-	    }
-	    if (success) {
-			  System.out.println("Renaming success - " + filename + " to " + newName);
-	    } else {
-			  System.out.println("Renaming error");
-	    }
 	}
 
 
